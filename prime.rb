c0e5fbd7eb1964d9d71d45d33d9d4c6906bf1381
Rosie Choi <rosiechoimanso@gmail.com>
2020-08-25 04:18:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,5 @@
 # Add  code here!
 def prime?(int)
-  if (2..int).each {|i| int % i } == 0
-    return FALSE
-  else if int < 2 
-    return FALSE
-  else
-    return TRUE
-  end
-end
-    
+  if int < 2
+    return 
 end
